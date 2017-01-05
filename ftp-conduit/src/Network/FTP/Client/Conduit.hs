911ee33dc5c7a2c961459b55295e0568f0f12779
Matt Robinson <mrobinson7627@gmail.com>
2017-01-04 23:10:57 -0500

    Start service refactor
    Trying to implement the service pattern for sending an receiving data.
    It's off to a good start in the normal client, but Conduit still needs
    work.

--- ftp-conduit/src/Network/FTP/Client/Conduit.hs
+++ ftp-conduit/src/Network/FTP/Client/Conduit.hs
@@ -7,7 +7,7 @@ module Network.FTP.Client.Conduit (
 import qualified Data.Conduit.Binary as CB
 import Data.Conduit
 import Control.Monad.IO.Class
-import System.IO
+import System.IO hiding (Handle)
 import Network.FTP.Client
     ( createDataSocket
     , sendCommand
@@ -15,41 +15,45 @@ import Network.FTP.Client
     , FTPCommand(..)
     , RTypeCode(..)
     , getLineResp
-    , ControlConnection(..)
-    , ccClose
-    , DataConnection(..)
-    , dcClose
     , createSendDataCommand
     , PortActivity(..)
     , getMultiLineResp
-    , sendLine
+    , sIOHandleImpl
     )
+
+import qualified Network.FTP.Client as FTP
 import qualified Data.ByteString as B
 import Data.ByteString (ByteString)
 import Control.Monad.Trans.Resource
 import Data.Monoid ((<>))
 
-getAllLineRespC :: MonadIO m => DataConnection -> Producer m ByteString
-getAllLineRespC (DC h) = loop
+data Handle m = Handle
+    { ftpHandle :: FTP.Handle
+    , source :: Producer m ByteString
+    , sink :: Consumer ByteString m ()
+    }
+
+getAllLineRespC :: MonadIO m => FTP.Handle -> Producer m ByteString
+getAllLineRespC h = loop
     where
         loop = do
-            eof <- liftIO $ hIsEOF h
+            eof <- liftIO $ isEOF h
             if eof
                 then return ()
                 else do
-                    line <- liftIO $ getLineResp h
+                    line <- liftIO $ FTP.recvLine h
                     yield line
                     loop
 
-sendAllLineC :: MonadIO m => DataConnection -> Consumer ByteString m ()
-sendAllLineC (DC h) = loop
+sendAllLineC :: MonadIO m => FTP.Handle -> Consumer ByteString m ()
+sendAllLineC h = loop
     where
         loop = do
             mx <- await
             case mx of
                 Nothing -> return ()
                 Just x -> do
-                    liftIO $ sendLine h x
+                    liftIO $ FTP.sendLine h x
                     loop
 
 sourceDataCommand
@@ -59,21 +63,25 @@ sourceDataCommand
     -> [FTPCommand]
     -> (DataConnection -> ConduitM i o m r)
     -> ConduitM i o m r
-sourceDataCommand cc@(CC ch) pa cmds f = do
-    x <- bracketP (createSendDataCommand cc pa cmds) dcClose f
-    resp <- liftIO $ getMultiLineResp ch
-    liftIO $ print $ "Recieved: " <> (show resp)
-    return x
+sourceDataCommand ch pa cmds f = bracketP
+    (createSendDataCommand cc pa cmds)
+    SIO.hClose
+    (\h -> do
+        x <- f $ sIOHandleImpl h
+        resp <- liftIO $ getMultiLineResp ch
+        liftIO $ print $ "Recieved: " <> (show resp)
+        return x
+    )
 
 nlst :: MonadResource m => ControlConnection -> [String] -> Producer m ByteString
 nlst cc args = sourceDataCommand cc Passive [RType TA, Nlst args] getAllLineRespC
 
 retr :: MonadResource m => ControlConnection -> String -> Producer m ByteString
-retr cc path = sourceDataCommand cc Passive [RType TI, Retr path] (\(DC h) -> CB.sourceHandle h)
+retr cc path = sourceDataCommand cc Passive [RType TI, Retr path] source
 
 stor :: MonadResource m => ControlConnection -> String -> RTypeCode -> Consumer ByteString m ()
 stor cc loc rtype = do
-    sourceDataCommand cc Passive [RType rtype, Stor loc] $ \dc@(DC h) ->
+    sourceDataCommand cc Passive [RType rtype, Stor loc] $ \dh ->
         case rtype of
-            TA -> sendAllLineC dc
-            TI -> CB.sinkHandle h
+            TA -> sendAllLineC dh
+            TI -> sink dh

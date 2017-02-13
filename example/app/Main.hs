f842a98e210ae4c5065db16de5306197145c87d3
Matt Robinson <mrobinson7627@gmail.com>
2017-02-12 22:09:33 -0500

    Added mlsd conduit

--- example/app/Main.hs
+++ example/app/Main.hs
@@ -1,8 +1,16 @@
 module Main where
 
 import Network.FTP.Client
+import qualified Network.FTP.Client.Conduit as FC
+import Conduit
+import Data.ByteString.Char8 (ByteString)
+import qualified Data.ByteString.Char8 as C
+import Data.Monoid ((<>))
 
 main :: IO ()
 main = withFTPS "hostname.com" 21 $ \h welcome -> do
     login h "username" "password"
-    print =<< mlsd h "."
+    runConduitRes
+        $ FC.mlsd h "."
+        .| mapC (C.pack . (<> "\n") . show)
+        .| stdoutC
